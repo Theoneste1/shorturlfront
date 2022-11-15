67c712127eb52c78733769ae9c459c75f776f041
theoneste <tnsanzabarinda17@alustudent.com>
2022-11-15 20:58:44 +0200

    added all files

---
+++ js.js
@@ -0,0 +1,21 @@
+function copyShorLink() {
+    // Get the text field
+    var copyText = document.getElementById("lname");
+    console.log(copyText.value)
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+  
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
+  } 
+
+  function shorltenLink(){
+    //get the field from the above link
+    var shortenLink=document.getElementById("fname").value;
+    document.getElementById("lname").value=shortenLink;
+  }
